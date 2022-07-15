0d9404a769ca632aa3778d978e3feea6bf5f1a70
Marius <marius@example.com>
2022-07-15 15:40:21 +0300

    form js controll

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 'use strict';
 console.log('main.js');
+
+//  nusitaikom
+const formEl = document.forms[0];
+
+formEl.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  console.log('js works');
+}
